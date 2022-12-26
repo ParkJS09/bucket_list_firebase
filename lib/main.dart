06fb358b9f7850289eac5173ebd76320c8cf0fb8
parkjs <python2_7@naver.com>
2022-12-26 22:41:05 +0900

    211226_bucket_service FireStore 적용 및 read create 기능 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:bucket_list_firebase/auth_service.dart';
+import 'package:bucket_list_firebase/bucket_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
@@ -12,6 +14,7 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AuthService()),
+        ChangeNotifierProvider(create: (context) => BucketService()),
       ],
       child: const MyApp(),
     ),
@@ -165,95 +168,113 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("버킷 리스트"),
-        actions: [
-          TextButton(
-            child: Text(
-              "로그아웃",
-              style: TextStyle(
-                color: Colors.white,
+    return Consumer<BucketService>(
+      builder: (context, bucketService, child) {
+        final authService = context.read<AuthService>();
+        User user = authService.currentUser()!;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("버킷 리스트"),
+            actions: [
+              TextButton(
+                child: Text(
+                  "로그아웃",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {
+                  print("sign out");
+                  context.read<AuthService>().signOut();
+                  // 로그인 페이지로 이동
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
               ),
-            ),
-            onPressed: () {
-              print("sign out");
-              context.read<AuthService>().signOut();
-              // 로그인 페이지로 이동
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-            },
+            ],
           ),
-        ],
-      ),
-      body: Column(
-        children: [
-          /// 입력창
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              children: [
-                /// 텍스트 입력창
-                Expanded(
-                  child: TextField(
-                    controller: jobController,
-                    decoration: InputDecoration(
-                      hintText: "하고 싶은 일을 입력해주세요.",
+          body: Column(
+            children: [
+              /// 입력창
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    /// 텍스트 입력창
+                    Expanded(
+                      child: TextField(
+                        controller: jobController,
+                        decoration: InputDecoration(
+                          hintText: "하고 싶은 일을 입력해주세요.",
+                        ),
+                      ),
                     ),
-                  ),
-                ),
 
-                /// 추가 버튼
-                ElevatedButton(
-                  child: Icon(Icons.add),
-                  onPressed: () {
-                    // create bucket
-                    if (jobController.text.isNotEmpty) {
-                      print("create bucket");
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-          Divider(height: 1),
-
-          /// 버킷 리스트
-          Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                String job = "$index";
-                bool isDone = false;
-                return ListTile(
-                  title: Text(
-                    job,
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: isDone ? Colors.grey : Colors.black,
-                      decoration: isDone
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none,
+                    /// 추가 버튼
+                    ElevatedButton(
+                      child: Icon(Icons.add),
+                      onPressed: () {
+                        // create bucket
+                        if (jobController.text.isNotEmpty) {
+                          print("create bucket");
+                          bucketService.create(jobController.text, user.uid);
+                        } else {
+                          // TODO 비어있는 리스트에 대한 noti 주기
+                        }
+                      },
                     ),
-                  ),
-                  // 삭제 아이콘 버튼
-                  trailing: IconButton(
-                    icon: Icon(CupertinoIcons.delete),
-                    onPressed: () {
-                      // 삭제 버튼 클릭시
-                    },
-                  ),
-                  onTap: () {
-                    // 아이템 클릭하여 isDone 업데이트
-                  },
-                );
-              },
-            ),
+                  ],
+                ),
+              ),
+              const Divider(height: 1),
+
+              /// 버킷 리스트
+              Expanded(
+                child: FutureBuilder<QuerySnapshot>(
+                  future: bucketService.read(user.uid),
+                  builder: (context, snapshot) {
+                    print(snapshot.hasData);
+                    final document = snapshot.data?.docs ?? [];
+                    return ListView.builder(
+                      itemCount: document.length,
+                      itemBuilder: (context, index) {
+                        final doc = document[index];
+                        String job = doc.get('job');
+                        bool isDone = doc.get('isDone');
+                        return ListTile(
+                          title: Text(
+                            job,
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: isDone ? Colors.grey : Colors.black,
+                              decoration: isDone
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                            ),
+                          ),
+                          // 삭제 아이콘 버튼
+                          trailing: IconButton(
+                            icon: Icon(CupertinoIcons.delete),
+                            onPressed: () {
+                              // 삭제 버튼 클릭시
+                            },
+                          ),
+                          onTap: () {
+                            // 아이템 클릭하여 isDone 업데이트
+                          },
+                        );
+                      },
+                    );
+                  }
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
